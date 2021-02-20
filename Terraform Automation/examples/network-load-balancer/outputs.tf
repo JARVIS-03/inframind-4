d695561029ed85e76be95e93acb69bf90db89d23
Kalai Yarasan <65356179+JARVIS-03@users.noreply.github.com>
2021-02-21 03:22:20 +0530

    Delete Terraform Automation directory

--- Terraform Automation/examples/network-load-balancer/outputs.tf
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# LOAD BALANCER OUTPUTS
-# ------------------------------------------------------------------------------
-
-output "load_balancer_ip_address" {
-  description = "Internal IP address of the load balancer"
-  value       = module.lb.load_balancer_ip_address
-}
--- Terraform Automation/examples/shared/startup_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-
-apt-get update
-apt -y install apache2
-apt -y install mysql-server
-sudo apt-get install git
